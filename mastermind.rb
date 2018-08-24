639cfbe0e5619e88c8057a2c6c3dd39382e42e62
seba <villeaho@tutanota.com>
2018-08-24 21:52:43 +0300

    Added add_color which takes a number and color. Player Class has a def color which takes a number and gets.chomps color then sends it to Board class. Moderator class launches the game and goes through 1 and 4 then displays the board.

--- mastermind.rb
+++ mastermind.rb
@@ -1,15 +1,51 @@
 class Board
-	@@board = "|1|2|3|4"
+	@@board = "1|2|3|4"
+	@@boards = []
 
 	def display
+		save_display
 		return @@board
 	end
 
-	def add_color
-		puts @@board.sub!("1","blue")
+	def boards
+		@@boards
 	end
-end	
 
-board = Board.new
-board.add_color
-puts board.display
+	def save_display
+		puts "@@board is #{@@board}"
+		@@boards.push(@@board)
+	end
+
+	def self.add_color(num, color)
+		@@board.sub!(num.to_s,color.to_s)
+	end
+end
+
+class Player
+	@@colors = ["red","grn","blu","ylw","blc","wht"]
+
+	def color(num)
+		puts "Input a color: #{@@colors}" 
+		color = gets.chomp
+		Board.add_color(num,color)
+	end
+end
+	
+class Moderator
+	def self.game
+		board = Board.new
+		player = Player.new
+		i = 0
+		puts board.display
+		while i < 12
+			player.color(1)
+			player.color(2)
+			player.color(3)
+			player.color(4)
+			puts board.display
+			p "Boards array is #{board.boards}"
+		end
+	end
+end
+
+Moderator.game
