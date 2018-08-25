e7084784c71c932313aec97dd62e2447075aa402
seba <villeaho@tutanota.com>
2018-08-25 23:32:46 +0300

    Made @@colors a hash instead of an array, so that each number represents a color. Player color def accepts only 1-6 and loops if anything else is given.

--- mastermind.rb
+++ mastermind.rb
@@ -3,47 +3,58 @@ class Board
 	@@boards = []
 
 	def display
-		save_display
 		return @@board
+	end
+
+	def board_reset
+		@@board = "1|2|3|4"
 	end
 
 	def boards
 		@@boards
 	end
 
-	def save_display
-		puts "@@board is #{@@board}"
-		@@boards.push(@@board)
-	end
-
-	def self.add_color(num, color)
-		@@board.sub!(num.to_s,color.to_s)
+	def self.add_color(num, color_num)
+		@colors = {1 => "red", 2 => "grn", 3 => "blu", 4 => "ylw", 5 => "blc", 6 => "wht"}
+		p @@boards.class
+		p @@boards.push("1|2|3|4")
+		@@boards.push(@@board.sub!(num.to_s,@colors[color_num])).to_s
 	end
 end
 
 class Player
-	@@colors = ["red","grn","blu","ylw","blc","wht"]
-
 	def color(num)
-		puts "Input a color: #{@@colors}" 
-		color = gets.chomp
-		Board.add_color(num,color)
+		nums = [1,2,3,4,5,6]
+		puts "Input a color number for position #{num}."
+		color_num = gets.chomp.to_i
+		until nums.include?(color_num)
+			puts "Input a valid color number for position #{num}."
+			color_num = gets.chomp.to_i
+
+		end
+		Board.add_color(num,color_num.to_i)
 	end
 end
 	
 class Moderator
+	@@colors = {1 => "red", 2 => "grn", 3 => "blu", 4 => "ylw", 5 => "blc", 6 => "wht"}
+
 	def self.game
 		board = Board.new
 		player = Player.new
 		i = 0
 		puts board.display
 		while i < 12
+			puts
+			puts @@colors
 			player.color(1)
 			player.color(2)
 			player.color(3)
 			player.color(4)
-			puts board.display
-			p "Boards array is #{board.boards}"
+			board.board_reset
+			p
+			puts
+			puts "Board: " + board.display
 		end
 	end
 end
